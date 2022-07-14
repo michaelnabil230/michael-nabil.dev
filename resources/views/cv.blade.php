bdc816273b575d5ae143a61aad05d04f7cf0697d
Michael Nabil <46572405+michaelnabil230@users.noreply.github.com>
2022-07-15 00:57:18 +0200

    Update my cv and my image

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -55,6 +55,22 @@
                 </li>
 
                 <li class="my-1 sm:w-1/2 md:w-1/3">
+                    <a class="group inline-flex items-center"
+                        href="https://wa.me/+201200954866?text=Hi%20Michael%20%0AI%20want%20to%20deal%20with%20you"
+                        target="_blank">
+                        <svg class="h-5 fill-current text-blue-500" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 448 512">
+                            <path
+                                d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
+                        </svg>
+                        <span class="ml-2 border-b border-dotted border-transparent group-hover:border-blue-500">
+                            +20 120 0954 866
+                            <small class="font-normal print:hidden">(Whatsapp only)</small>
+                        </span>
+                    </a>
+                </li>
+
+                <li class="my-1 sm:w-1/2 md:w-1/3">
                     <a class="group inline-flex items-center" href="https://michael-nabil.dev" target="_blank">
                         <svg class="h-5 fill-current text-blue-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
                             <path
@@ -217,7 +233,7 @@
             <div
                 class="
                     inline-block p-2 bg-blue-100 rounded-lg text-blue-700 font-bold text-center leading-none tracking-wide
-                    sm:h-20 sm:w-20 sm:p-0 sm:flex-shrink-0 sm:flex sm:items-center sm:justify-center sm:text-lg
+                    sm:h-24 sm:w-20 sm:p-0 sm:flex-shrink-0 sm:flex sm:items-center sm:justify-center sm:text-lg
                 ">
                 <div>
                     <span class="inline sm:block">now</span>
@@ -231,7 +247,7 @@
                 <p class="text-lg font-bold">Co-founder and Lead Developer</p>
                 <p>
                     <a class="text-blue-700 font-semibold" href="https://shaghf.sa" target="_blank">
-                        Shaghf
+                        Shaghf.sa
                     </a>
                 </p>
                 <ul class="mt-3 pl-4 list-disc">
@@ -249,7 +265,7 @@
             <div
                 class="
                     inline-block p-2 bg-blue-100 rounded-lg text-blue-700 font-bold text-center leading-none tracking-wide
-                    sm:h-20 sm:w-20 sm:p-0 sm:flex-shrink-0 sm:flex sm:items-center sm:justify-center sm:text-lg
+                    sm:h-24 sm:w-20 sm:p-0 sm:flex-shrink-0 sm:flex sm:items-center sm:justify-center sm:text-lg
                 ">
                 <div>
                     <span class="inline sm:block">2021</span>
@@ -261,7 +277,11 @@
 
             <div class="mt-2 sm:mt-0 sm:ml-6">
                 <p class="text-lg font-bold">Senior Web Developer</p>
-                <p class="font-semibold">Sofate</p>
+                <p>
+                    <a class="text-blue-700 font-semibold" href="https://sofate.net" target="_blank">
+                        Sofate.net
+                    </a>
+                </p>
                 <ul class="mt-3 pl-4 list-disc">
                     <li>Back-end web development.</li>
                 </ul>
@@ -272,7 +292,7 @@
             <div
                 class="
                     inline-block p-2 bg-blue-100 rounded-lg text-blue-700 font-bold text-center leading-none tracking-wide
-                    sm:h-20 sm:w-20 sm:p-0 sm:flex-shrink-0 sm:flex sm:items-center sm:justify-center sm:text-lg
+                    sm:h-24 sm:w-20 sm:p-0 sm:flex-shrink-0 sm:flex sm:items-center sm:justify-center sm:text-lg
                 ">
                 <div>
                     <span class="inline sm:block">2020</span>
@@ -284,7 +304,11 @@
 
             <div class="mt-2 sm:mt-0 sm:ml-6">
                 <p class="text-lg font-bold">Senior Web Developer</p>
-                <p class="font-semibold">M6twer</p>
+                <p>
+                    <a class="text-blue-700 font-semibold" href="https://m6twer.com" target="_blank">
+                        M6twer.com
+                    </a>
+                </p>
                 <ul class="mt-3 pl-4 list-disc">
                     <li>Back-end web development.</li>
                 </ul>
@@ -295,7 +319,7 @@
             <div
                 class="
                     inline-block p-2 bg-blue-100 rounded-lg text-blue-700 font-bold text-center leading-none tracking-wide
-                    sm:h-20 sm:w-20 sm:p-0 sm:flex-shrink-0 sm:flex sm:items-center sm:justify-center sm:text-lg
+                    sm:h-24 sm:w-20 sm:p-0 sm:flex-shrink-0 sm:flex sm:items-center sm:justify-center sm:text-lg
                 ">
                 <div>
                     <span class="inline sm:block">2019</span>
@@ -307,7 +331,9 @@
 
             <div class="mt-2 sm:mt-0 sm:ml-6">
                 <p class="text-lg font-bold">Web Developer</p>
-                <p class="font-semibold">Freelancer</p>
+                <p class="text-blue-700 font-semibold">
+                    Freelancer
+                </p>
                 <ul class="mt-3 pl-4 list-disc">
                     <li>Back-end web development.</li>
                     <li>
